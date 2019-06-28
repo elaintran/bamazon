727ea339913c00844a8d1fe51cf9532aa66f43fc
elaintran <elain.ttran@gmail.com>
2019-06-27 21:44:49 -0500

    inserted mysql data into cli table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 //node packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table3");
 var chalk = require("chalk");
 
 var connection = mysql.createConnection({
@@ -15,6 +16,41 @@ connection.connect(function(error) {
     if (error) {
         console.log(error)
     }
-    console.log("yo");
-    connection.end();
+    productDisplay();
+    //console.log("yo");
+    //connection.end();
 })
+
+function productDisplay() {
+    connection.query("SELECT * FROM products", function(error, response) {
+        if (error) {
+            console.log(error);
+        }
+        var table = new Table({
+            head: [chalk.white("ID"), chalk.white("Product"),
+                   chalk.white("Department"), chalk.white("Price"),
+                   chalk.white("Stock")], 
+            colWidths: [5, 15, 15, 10, 10]
+        });
+        var stockQuanity;
+        for (var i = 0; i < response.length; i++) {
+            // console.log(typeof(response[i].stock_quanity));
+            var responseStock = response[i].stock_quanity;
+            // stockQuanity.push(responseStock);
+            // console.log(stockQuanity);
+            if (responseStock === 0) {
+                stockQuanity = chalk.red(responseStock);
+            } else if (responseStock >= 1 && responseStock < 6) {
+                stockQuanity = chalk.yellow(responseStock);
+            } else {
+                stockQuanity = chalk.green(responseStock);
+            }
+            table.push(
+                [response[i].item_id, response[i].product_name, response[i].department_name,
+                "$" + response[i].price, stockQuanity]
+            );
+        }
+        console.log(table.toString());
+        connection.end();
+    })
+}
