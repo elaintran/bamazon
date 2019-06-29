55727b4368d4ad340e0751c319fc99a928da7b33
elaintran <elain.ttran@gmail.com>
2019-06-28 19:46:04 -0500

    adjusted nullable columns

--- bamazonSeed.sql
+++ bamazonSeed.sql
@@ -8,8 +8,8 @@ CREATE TABLE products (
 	item_id INTEGER AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(30) NOT NULL,
     department_name VARCHAR(30) NULL,
-    price DECIMAL(10, 2) NULL,
-    stock_quantity INTEGER(10) NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INTEGER(10) NOT NULL,
     PRIMARY KEY (item_id)
 );
 
